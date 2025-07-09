1fefc6606b6561b7e9d96cf68cf95f76816be9b1
SatoshiInoue <satoshi.inoue@melodysquare.com>
2025-07-09 14:00:09 +0900

    Marquee updates, removed boolean

--- blocks/simple-marquee/simple-marquee.js
+++ blocks/simple-marquee/simple-marquee.js
@@ -1,8 +1,8 @@
 import { readBlockConfig } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const config = readBlockConfig(block);
-  console.log('marquee');
-  console.log(config);
-  console.log(config.boolean);
+  // const config = readBlockConfig(block);
+  // console.log('marquee');
+  // console.log(config);
+  // console.log(config.boolean);
 }
